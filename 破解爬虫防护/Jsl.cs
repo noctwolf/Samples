8ad74a12e0ebff469daa46065aa515993ca4bb8b
noctwolf <noctwolf@gmail.com>
2019-04-23 23:02:41 +0800

    破解爬虫防护

---
+++ 破解爬虫防护/Jsl.cs
@@ -0,0 +1,50 @@
+﻿using Jint;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net;
+using System.Text;
+using Raize.CodeSiteLogging;
+
+namespace 破解爬虫防护
+{
+    public static class Jsl
+    {
+        public static string GetCookies(HttpWebResponse httpWebResponse)
+        {
+            string script1 = "";
+            string script2 = "";
+            try
+            {
+                using (var responseStream = httpWebResponse.GetResponseStream())
+                using (var streamReader = new StreamReader(responseStream ?? throw new InvalidOperationException(), Encoding.UTF8))
+                    script1 = streamReader.ReadToEnd();
+                var engine = new Engine().SetValue("document", new Document());
+
+                var run = script1.Substring("<script>", "</script>").Replace("{eval(", "{return (");
+                run = $"function script2(){{{run}}};";
+                script2 = engine.Execute(run).GetValue("script2").Invoke().ToString();
+
+                var del = script2.Substring("=function(){", "document.cookie=");
+                run = script2.Replace(del + "document.cookie=", "return ");
+                var name = script2.Substring("var ", "=");
+                return engine.Execute(run).GetValue(name).Invoke().ToString();
+            }
+            catch (Exception ex)
+            {
+                ex.SendCodeSite();
+                CodeSite.Send("script1", script1);
+                CodeSite.Send("script2", script2);
+                return "";
+            }
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+        private class Document
+        {
+            public void attachEvent(object o1, object o2) { }
+        }
+    }
+}
---
+++ 破解爬虫防护/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Raize.CodeSiteLogging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ScrapySharp.Network;
+
+namespace 破解爬虫防护
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(Test);
+        }
+
+        private void Test()
+        {
+            CodeSite.EnterMethod(this, "Test");
+            ScrapingBrowser scrapingBrowser = new ScrapingBrowser();
+            scrapingBrowser.IgnoreCookies = true;
+        re:
+            try
+            {
+                var s = scrapingBrowser.NavigateTo(new Uri("https://www.yidaiyilu.gov.cn/info/iList.jsp?cat_id=10009"), HttpVerb.Get, "");
+                CodeSite.Send("s", s);
+            }
+            catch (Exception ex)
+            {
+                if (ex is WebException we && we.Response is HttpWebResponse hwr)
+                {
+                    if ((int)hwr.StatusCode == 521)
+                    {
+                        CodeSite.SendReminder("破解爬虫防护");
+                        scrapingBrowser.SetCookies(new Uri("https://www.yidaiyilu.gov.cn/"), Jsl.GetCookies(hwr));
+                        goto re;
+                    }
+                }
+            }
+            finally
+            {
+                CodeSite.ExitMethod(this, "Test");
+            }
+        }
+    }
+}

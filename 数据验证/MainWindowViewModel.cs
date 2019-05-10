d975ab4946da66462c7109a077deeab53c49bfae
noctwolf <noctwolf@gmail.com>
2019-05-10 17:43:38 +0800

    数据验证

---
+++ 数据验证/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace 数据验证
+{
+    [POCOViewModel(ImplementIDataErrorInfo = true)]
+    public class MainWindowViewModel
+    {
+        [Required(ErrorMessage = "请输入用户名")]
+        public virtual string UserName { get; set; }
+    }
+}

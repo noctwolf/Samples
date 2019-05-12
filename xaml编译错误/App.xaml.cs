7a985f7c3da8e0a88220bd6b26bbfa73a6bf8769
noctwolf <noctwolf@gmail.com>
2019-05-13 02:32:15 +0800

    xaml编译错误

---
+++ xaml编译错误/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace xaml编译错误
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ xaml编译错误/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace xaml编译错误
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            label.Content = Properties.Resources.Border;
+        }
+    }
+}

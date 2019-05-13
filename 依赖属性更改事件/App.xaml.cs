640e5a020c14f397fa0c3f9a5a9fd5afbb5f25d7
noctwolf <noctwolf@gmail.com>
2019-05-13 23:06:45 +0800

    依赖属性更改事件

---
+++ 依赖属性更改事件/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace 依赖属性更改事件
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 依赖属性更改事件/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace 依赖属性更改事件
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DependencyPropertyDescriptor.FromProperty(TextBox.TextProperty, typeof(TextBox))
+                .AddValueChanged(textBox, TextChanged);
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+            textBlock.Text = textBox.Text;
+        }
+    }
+}

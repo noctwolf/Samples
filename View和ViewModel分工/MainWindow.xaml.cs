1b68e3c6285a02a1477ce0f321ef47c59aa81f39
noctwolf <noctwolf@gmail.com>
2019-06-28 10:08:52 +0800

    View和ViewModel分工

---
+++ View和ViewModel分工/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace View和ViewModel分工
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (sender is Button button && openFileDialog.ShowDialog() == true)
+                button.CommandParameter = openFileDialog.FileName;
+            else
+                throw new UserCanceledException();
+        }
+    }
+}
---
+++ View和ViewModel分工/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View和ViewModel分工
+{
+    public class MainWindowViewModel
+    {
+        public virtual void ParseFile(string fileName)
+        {
+        }
+    }
+}

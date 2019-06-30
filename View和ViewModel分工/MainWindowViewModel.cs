b57b49931e46479f4c1aafe5ca69af75f367bd24
noctwolf <noctwolf@gmail.com>
2019-06-30 16:21:39 +0800

    移除using和virtual

--- View和ViewModel分工/MainWindowViewModel.cs
+++ View和ViewModel分工/MainWindowViewModel.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace View和ViewModel分工
+﻿namespace View和ViewModel分工
 {
     public class MainWindowViewModel
     {
-        public virtual void ParseFile(string fileName)
+        public void ParseFile(string fileName)
         {
+            //解析文件
         }
     }
 }

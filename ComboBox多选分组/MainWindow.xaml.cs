542c32973c7f43ff377fd121d853b45bf95755a0
noctwolf <noctwolf@gmail.com>
2019-07-03 02:05:56 +0800

    ComboBox多选分组

---
+++ ComboBox多选分组/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBox多选分组
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<Demo> DemoList { get; } = Enumerable.Range(0, 10).Select(f => new Demo { Title = f, Group = f / 4 }).ToList();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var myView = (CollectionView)CollectionViewSource.GetDefaultView(DemoList);
+            if (myView.CanGroup) myView.GroupDescriptions.Add(new PropertyGroupDescription("Group"));
+        }
+    }
+
+    public struct Demo
+    {
+        public int Title { get; set; }
+
+        public int Group { get; set; }
+    }
+}

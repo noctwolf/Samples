8f802b6b92f3e1d11c22c1d9fb08c3ed7051c735
noctwolf <noctwolf@gmail.com>
2019-07-19 17:39:27 +0800

    多UI线程

---
+++ 多UI线程/LoadingWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace 多UI线程
+{
+    /// <summary>
+    /// LoadingWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingWindow : Window
+    {
+        private Timer timer;
+
+        public LoadingWindow()
+        {
+            InitializeComponent();
+            timer = new Timer(
+                _ => Dispatcher.InvokeAsync(() => textBlock.Text = DateTime.Now.ToString("HH:mm:ss.ffff")),
+                null, 0, 10);
+        }
+    }
+}
---
+++ 多UI线程/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace 多UI线程
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void 普通方式_Click(object sender, RoutedEventArgs e)
+        {
+            var loadingWindow = new LoadingWindow();
+            loadingWindow.Show();
+            耗时操作();
+            loadingWindow.Close();
+        }
+
+        private static void 耗时操作()
+        {
+            for (var i = 0; i < 1000; i++) Thread.Sleep(10);
+        }
+
+        private void 单独UI线程_Click(object sender, RoutedEventArgs e)
+        {
+            LoadingWindow loadingWindow = null;
+            Thread thread = new Thread(() =>
+            {
+                loadingWindow = new LoadingWindow();
+                loadingWindow.Show();
+                System.Windows.Threading.Dispatcher.Run();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+            耗时操作();
+            loadingWindow.Dispatcher.Invoke(() =>
+            {
+                loadingWindow.Close();
+                System.Windows.Threading.Dispatcher.CurrentDispatcher.InvokeShutdown();
+            });
+        }
+    }
+}

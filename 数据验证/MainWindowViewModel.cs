1cc63f88289df4395857e03f0fef66789869dfdb
noctwolf <noctwolf@gmail.com>
2019-07-23 21:03:45 +0800

    HasErrors

--- 数据验证/MainWindowViewModel.cs
+++ 数据验证/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using DevExpress.Mvvm.DataAnnotations;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace 数据验证
 {
@@ -8,5 +10,12 @@ namespace 数据验证
     {
         [Required(ErrorMessage = "请输入用户名")]
         public virtual string UserName { get; set; }
+
+        public void Submit() { }
+
+        public bool CanSubmit() => !HasErrors;
+
+        public bool HasErrors => this is IDataErrorInfo dataErrorInfo
+                && GetType().GetProperties().Any(f => !string.IsNullOrEmpty(dataErrorInfo[f.Name]));
     }
 }

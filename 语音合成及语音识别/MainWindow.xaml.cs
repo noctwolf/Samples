a6096159fb77b2225ede04ecd49767ae9fe09ca7
noctwolf <noctwolf@gmail.com>
2022-02-08 16:13:29 +0800

    语义识别

--- 语音合成及语音识别/MainWindow.xaml.cs
+++ 语音合成及语音识别/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using System.Windows;
+using System.Windows.Media;
 
 namespace 语音合成及语音识别
 {
@@ -30,6 +31,11 @@ namespace 语音合成及语音识别
             Grammar grammar = new Grammar(new GrammarBuilder("线程")) { Name = "命令" };//没有这个，通常识别为“县城”
             recognizer.LoadGrammar(grammar);
 
+            var gb = new GrammarBuilder("设置背景");
+            gb.Append(new SemanticResultKey("Color", new Choices(new string[] { "红色", "绿色", "蓝色" })));
+            grammar = new Grammar(gb) { Name = "设置背景颜色" };
+            recognizer.LoadGrammar(grammar);
+
             grammar = new Grammar(new GrammarBuilder("停止识别")) { Name = "Cancel" };
             recognizer.LoadGrammar(grammar);
 
@@ -45,6 +51,21 @@ namespace 语音合成及语音识别
                         break;
                     case "命令":
                         s = $"识别到命令，{s}";
+                        break;
+                    case "设置背景颜色":
+                        s = $"识别到命令，{s}";
+                        switch (e1.Result.Semantics["Color"].Value)
+                        {
+                            case "红色":
+                                Background = Brushes.Red;
+                                break;
+                            case "绿色":
+                                Background = Brushes.Green;
+                                break;
+                            case "蓝色":
+                                Background = Brushes.Blue;
+                                break;
+                        }
                         break;
                 }
                 listBox.Items.Insert(0, s);

9d835fb412f677a80ccd4ce2a5cfcb35f7fd6fd3
noctwolf <noctwolf@gmail.com>
2022-02-09 16:29:05 +0800

    username

--- 语音合成及语音识别/App.xaml.cs
+++ 语音合成及语音识别/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace 语音合成及语音识别
